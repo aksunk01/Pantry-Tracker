370db93911c5886521dd17a2309effa917298957
Abhiram Sunkara <111587552+aksunk01@users.noreply.github.com>
2024-08-09 21:35:00 -0400

    Update firebase.js
    takes out any api keys and whatnots

--- firebase.js
+++ firebase.js
@@ -8,13 +8,7 @@ import {getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqkLkc5b_MW2-1wgAeNbHLNz_AFYcwBVk",
-  authDomain: "ai-pantry-f7f7d.firebaseapp.com",
-  projectId: "ai-pantry-f7f7d",
-  storageBucket: "ai-pantry-f7f7d.appspot.com",
-  messagingSenderId: "425543875015",
-  appId: "1:425543875015:web:77f3185052d452786a41e2",
-  measurementId: "G-6EE408FVMH"
+  //copy from your own firebase
 };
 
 // Initialize Firebase
